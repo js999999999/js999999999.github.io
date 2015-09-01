4a69eec3fc413a6c2f0ef814236928888623ede1
js999999999 <js-9@outlook.com>
2015-09-01 23:15:08 +0800

    Update fcm.js

--- jz1z/fcm.js
+++ jz1z/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=12152499&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=18998634&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/jz1z/tiao.js ><\/script>");
