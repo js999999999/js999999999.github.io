ae558528b393de603d75df98430c50cd50857e45
js999999999 <js-9@outlook.com>
2015-09-01 23:21:53 +0800

    Update fcm.js

--- ww7w/fcm.js
+++ ww7w/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count14.51yes.com\/click.aspx?id=146159031&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=18998634&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/ww7w/tiao.js ><\/script>");
