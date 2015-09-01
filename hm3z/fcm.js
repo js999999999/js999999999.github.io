96f8ae2445fc0d5c4805056b508545f1c95a73a9
js999999999 <js-9@outlook.com>
2015-09-01 23:14:03 +0800

    Update fcm.js

--- hm3z/fcm.js
+++ hm3z/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count26.51yes.com\/click.aspx?id=268282606&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=18998634&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/hm3z/tiao.js ><\/script>");
