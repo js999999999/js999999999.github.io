16beaf899ebd0f9461c693ad4dad32302cc51001
js999999999 <js-9@outlook.com>
2015-09-02 09:17:11 +0800

    Update fcm.js

--- qe4z/fcm.js
+++ qe4z/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count33.51yes.com\/click.aspx?id=335610164&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=18998634&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/qe4z/tiao.js ><\/script>");
