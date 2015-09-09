ccdfd61fd5d97fb2cb91bda1066779f86880a275
js999999999 <js-9@outlook.com>
2015-09-09 18:30:25 +0800

    Update fcm.js

--- 00448/fcm.js
+++ 00448/fcm.js
@@ -1,4 +1,4 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=12604096&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count44.51yes.com\/click.aspx?id=441663749&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/00448/tiao.js ><\/script>");
 
