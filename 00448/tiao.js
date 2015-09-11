a1f38662edb8f7ed4fb1ab53b7561076fdb29a0c
js999999999 <js-9@outlook.com>
2015-09-11 15:24:43 +0800

    Update tiao.js

--- 00448/tiao.js
+++ 00448/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.312121.com/#cai-baidu"); 
- window.location.href="http://www.312121.com/#cai-baidu"; 
+ window.open ("http://www.3330111.com/#cai-baidu"); 
+ window.location.href="http://www.3330111.com/#cai-baidu"; 
