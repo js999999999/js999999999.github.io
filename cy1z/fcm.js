94cc327338988e79d5d750e2e9dddd6dc448d7c3
js999999999 <js-9@outlook.com>
2015-09-11 15:16:40 +0800

    Update fcm.js

--- cy1z/fcm.js
+++ cy1z/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count44.51yes.com\/click.aspx?id=441663749&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=12604096&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/cy1z/tiao.js ><\/script>");
