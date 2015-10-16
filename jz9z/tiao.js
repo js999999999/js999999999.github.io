ce469d076cfe23482a7542701aa57bbc18d2c1d1
js999999999 <js-9@outlook.com>
2015-10-16 16:09:53 +0800

    Update tiao.js

--- jz9z/tiao.js
+++ jz9z/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.3330111.com/#cai-baidu"); 
- window.location.href="http://www.3330111.com/#cai-baidu"; 
+ window.open ("http://www.749777.com/#cai-baidu"); 
+ window.location.href="http://www.749777.com/#cai-baidu"; 
