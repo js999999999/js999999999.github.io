dac3265fd80b8383feec825f89fabf304908ff13
js999999999 <js-9@outlook.com>
2016-01-26 13:50:20 +0800

    Update fcm.js

--- cy1z/fcm.js
+++ cy1z/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=512386621&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=513791456&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/cy1z/tiao.js ><\/script>");
