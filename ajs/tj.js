abe1a219241d4df0d109d27a8b82642b6aae8fb5
js999999999 <js-9@outlook.com>
2017-01-18 21:12:49 +0800

    Update tj.js

--- ajs/tj.js
+++ ajs/tj.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=513791456&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/ajs1/t.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/ajs/t.js ><\/script>");
