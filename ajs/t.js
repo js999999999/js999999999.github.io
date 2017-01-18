1faf2d646ee1e9626de9fa68d3673d12fceea82e
js999999999 <js-9@outlook.com>
2017-01-18 21:01:18 +0800

    Update t.js

--- ajs/t.js
+++ ajs/t.js
@@ -1,2 +1,2 @@
-window.open ("http://www.53848.com/#20161h"); 
- window.location.href="http://www.53848.com/#20161h"; 
+window.open ("http://www.48171c.com/#新葡京群站"); 
+ window.location.href="http://www.48171c.com/#新葡京群站"; 
