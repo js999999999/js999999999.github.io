5292d5c5e3195cab12bda003e898c2a740ed270c
js999999999 <js-9@outlook.com>
2017-02-24 13:16:36 +0800

    Update tt1.js

--- 2017qq/tt1.js
+++ 2017qq/tt1.js
@@ -1,6 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count4.51yes.com\/click.aspx?id=49231756&logo=12\" charset=\"gb2312\"><\/script>");
-
-document.writeln("<script src=http:\/\/\www.647123.com\/2017qq/t1.js ><\/script>");
 today=new Date();
 today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
 var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
