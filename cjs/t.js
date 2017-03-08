67461360aae52ff40b4e3b7cd859b7c8be412ce1
js999999999 <js-9@outlook.com>
2017-03-08 23:45:47 +0800

    Update t.js

--- cjs/t.js
+++ cjs/t.js
@@ -1,2 +1,2 @@
-window.open ("http://www.53848.com/#澳门永利（特码48.5倍）"); 
- window.location.href="http://www.53848.com/#澳门永利（特码48.5倍）"; 
+window.open ("http://www.58709.com/#澳门新葡京（特码48.5倍）"); 
+ window.location.href="http://www.58709.com/#澳门新葡京（特码48.5倍）"; 
