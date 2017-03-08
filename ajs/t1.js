daeaf5a93d4e5945515bff406dcdedf214116a1b
js999999999 <js-9@outlook.com>
2017-03-08 23:49:58 +0800

    Update t1.js

--- ajs/t1.js
+++ ajs/t1.js
@@ -1,2 +1,2 @@
-window.open ("http://www.48171c.com/#新葡京群站"); 
- window.location.href="http://www.48171c.com/#澳门新葡京大型国际娱乐城"; 
+window.open ("http://www.58709.com/#澳门新葡京（特码48.5倍）"); 
+ window.location.href="http://www.58709.com/#澳门新葡京（特码48.5倍）"; 
