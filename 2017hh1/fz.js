a1f30862559995536b2bf571df8d281c79733014
js999999999 <js-9@outlook.com>
2017-04-26 15:22:40 +0800

    Delete fz.js

--- 2017hh1/fz.js
+++
@@ -1,3 +0,0 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count40.51yes.com\/click.aspx?id=407817877&logo=12\" charset=\"gb2312\"><\/script>");
-
-document.writeln("<script src=http:\/\/\www.647123.com\/2017hh/t.js ><\/script>");
