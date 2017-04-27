55bc8cb91e4107d4b2b33081e2af4b001dcea682
js999999999 <js-9@outlook.com>
2017-04-27 15:54:52 +0800

    Update t.js

--- 2017cc/t.js
+++ 2017cc/t.js
@@ -1,2 +1,2 @@
-window.open ("http://www.00585.com/#新濠天地-老品牌"); 
-window.location.href="http://www.00585.com/#新濠天地-老品牌"; 
+window.open ("http://www.24499.com/#澳门永利（特码48.5倍）"); 
+window.location.href="http://www.24499.com/#澳门永利（特码48.5倍）"; 
