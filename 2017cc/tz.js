277c24e84d50fab6ec601921a76919fdbc799904
js999999999 <js-9@outlook.com>
2017-04-27 16:01:35 +0800

    Update tz.js

--- 2017cc/tz.js
+++ 2017cc/tz.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count37.51yes.com\/click.aspx?id=375873618&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count37.51yes.com\/click.aspx?id=378995920&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/2017cc/t.js ><\/script>");
