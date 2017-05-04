6ac801074c1bcdbbf5aea621cc18b2761e3b9901
js999999999 <js-9@outlook.com>
2017-05-04 19:22:10 +0800

    Update t1.js

--- 2017qq/t1.js
+++ 2017qq/t1.js
@@ -1,3 +1,3 @@
-window.open ("http://www.58709.com/#澳门新葡京（特码48.5倍）"); 
+window.open ("http://www.027067.com/#澳门新葡京（特码48.5倍）"); 
  
-window.location.href="http://www.58709.com/#澳门新葡京（特码48.5倍）"; 
+window.location.href="http://www.027067.com/#澳门新葡京（特码48.5倍）"; 
