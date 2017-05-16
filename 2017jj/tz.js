403793eb56bc48310bf67150556f1989d4e3e629
js999999999 <js-9@outlook.com>
2017-05-16 14:33:21 +0800

    Update tz.js

--- 2017jj/tz.js
+++ 2017jj/tz.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count50.51yes.com\/click.aspx?id=503753931&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/2017jj/t.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/2017jj1/t.js ><\/script>");
