21a5d7db8eec441809c848b46746c33f98a73fd5
js999999999 <js-9@outlook.com>
2017-06-20 11:51:40 +0800

    Update tt2.js

--- 2017hh/tt2.js
+++ 2017hh/tt2.js
@@ -7,5 +7,5 @@ document.write ('<script language="javascript" src="http://count47.51yes.com/cli
 }
 else
 {
-window.location.href="http://www.24499.com/#澳门永利（特码48.5倍）";
+window.location.href="http://www.24499.com/#永利（特码48.5倍）";
 }
