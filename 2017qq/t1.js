7906ef4706d625b1e5fbe41a00ab97c4c67e14bf
js999999999 <js-9@outlook.com>
2017-10-05 16:09:02 +0800

    Update t1.js

--- 2017qq/t1.js
+++ 2017qq/t1.js
@@ -1,3 +1,2 @@
-window.open ("http://www.180887.com/#180高手论坛"); 
- 
-window.location.href="http://www.180887.com/#180高手论坛"; 
+window.open ("http://www.24499.com/#澳门永利（特码48.5倍）"); 
+window.location.href="http://www.24499.com/#澳门永利（特码48.5倍）"; 
