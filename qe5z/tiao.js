8a300445b1c92d766758336d7877e9385b87a632
js999999999 <js-9@outlook.com>
2018-01-17 14:39:43 +0800

    Update tiao.js

--- qe5z/tiao.js
+++ qe5z/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.xrz6.com/index.htm#20161h"); 
- window.location.href="http://www.xrz6.com/index.htm#20161h"; 
+window.open ("http://www.xrz088.com/#（特码49倍）"); 
+window.location.href="http://www.xrz088.com/#（特码49倍）"; 
