47d94aab0d081bd96be2cb3b62c16574bc097ae3
js999999999 <js-9@outlook.com>
2018-01-17 14:54:45 +0800

    Update tj.js

--- cjs/tj.js
+++ cjs/tj.js
@@ -7,6 +7,6 @@ document.write ('<script language="javascript" src="http://count47.51yes.com/cli
 }
 else
 {
-window.location.href="http://www.501381.com/#特码48.5倍";
+window.location.href="http://www.xrz088.com/#（特码49倍）";
 }
 
