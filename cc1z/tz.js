8fc0d834f4f984eec8caaa9cff8d0978d92a7b49
js999999999 <js-9@outlook.com>
2018-01-17 14:04:31 +0800

    Update tz.js

--- cc1z/tz.js
+++ cc1z/tz.js
@@ -1,2 +1,2 @@
-window.open ("http://www.678987.com/#澳门永利（特码48.5倍）"); 
-window.location.href="http://www.678987.com/#澳门永利（特码48.5倍）"; 
+window.open ("http://www.501381.com/#特码48.5倍"); 
+window.location.href="http://www.501381.com/#特码48.5倍"; 
