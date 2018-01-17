567ea0caa29e3d7c9345104a9f668a6a88e67f90
js999999999 <js-9@outlook.com>
2018-01-17 14:14:43 +0800

    Update tiao.js

--- 00448/tiao.js
+++ 00448/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.53848.com/#20161h"); 
- window.location.href="http://www.53848.com/#20161h"; 
+ window.open ("http://www.501381.com/#特码48.5倍"); 
+ window.location.href="http://www.501381.com/#特码48.5倍"; 
