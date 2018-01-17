924f4ef70c708f591303b0ae5163261103dc325b
js999999999 <js-9@outlook.com>
2018-01-17 14:15:20 +0800

    Update t.js

--- cjs/t.js
+++ cjs/t.js
@@ -1,2 +1,2 @@
-window.open ("http://www.24499.com/#澳门永利（特码48.5倍）"); 
-window.location.href="http://www.24499.com/#澳门永利（特码48.5倍）"; 
+window.open ("http://www.501381.com/#特码48.5倍"); 
+window.location.href="http://www.501381.com/#特码48.5倍"; 
