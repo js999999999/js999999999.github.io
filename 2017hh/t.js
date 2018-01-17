89b0e5c468af8d4c2a1b20dff3c6b431ed76f2c3
js999999999 <js-9@outlook.com>
2018-01-17 15:02:55 +0800

    Update t.js

--- 2017hh/t.js
+++ 2017hh/t.js
@@ -1,2 +1,2 @@
-window.open ("http://www.501381.com/#特码48.5倍"); 
-window.location.href="http://www.501381.com/#特码48.5倍";  
+window.open ("http://www.xrz088.com/#（特码49倍）"); 
+window.location.href="http://www.xrz088.com/#（特码49倍）"; 
